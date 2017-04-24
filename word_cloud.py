478ed9d601658f95c950dbeb49dfb56925cd17c7
Aki Ariga <ariga@cloudera.com>
2017-04-24 01:28:22 +0000

    Add wordcloud

---
+++ word_cloud.py
@@ -0,0 +1,52 @@
+## NLP example: Wordcloud of "alice in wonder land"
+# Basic code is taken from https://github.com/amueller/word_cloud/blob/master/examples/masked.py
+
+from pyspark.sql import SparkSession
+from wordcloud import WordCloud, STOPWORDS
+
+def word_tokenize(x):
+  import MeCab as mc
+  t = mc.Tagger("-Owakati -d ./MECAB/mecab_env/lib/mecab/dic/ipadic")
+  return t.parse(x)
+
+spark = SparkSession.builder \
+      .appName("Word count") \
+      .getOrCreate()
+    
+threshold = 5
+
+# Download from http://www.genpaku.org/alice01/alice01j.txt
+# Written by: Lewis Carroll
+# Translated by: Hiroo Yamagata
+
+text_file = spark.sparkContext.textFile("/tmp/alice01j.txt")
+
+stopwords = set(STOPWORDS)
+
+counts = text_file.map(word_tokenize) \
+             .flatMap(lambda line: line.split(" ")) \
+             .filter(lambda word: word not in stopwords) \
+             .map(lambda word: (word.lower(), 1)) \
+             .reduceByKey(lambda a, b: a + b)
+
+from pyspark.sql.types import *
+schema = StructType([StructField("word", StringType(), True),
+                     StructField("frequency", IntegerType(), True)])
+
+filtered = counts.filter(lambda pair: pair[1] >= threshold)
+counts_df = spark.createDataFrame(filtered, schema)
+
+frequencies = counts_df.toPandas().set_index('word').T.to_dict('records')
+
+from os import path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+alice_mask = np.array(Image.open(path.join("resources", "alice-mask.jpg")))
+
+wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
+               stopwords=stopwords)
+wc.generate_from_frequencies(dict(*frequencies))
+
+plt.imshow(wc, interpolation='bilinear')
