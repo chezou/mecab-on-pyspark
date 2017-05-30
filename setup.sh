e89f83221129ad702ed68fc701d8bdab1c983de6
Michiaki Ariga <ariga@cloudera.com>
2017-05-30 05:37:09 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install wordcloud
+
 conda create -n mecab_env --copy -c chezou -y -q python=2 mecab
 
 source activate mecab_env
@@ -12,8 +14,9 @@ zip -r ../../mecab_env.zip mecab_env
 wget https://noto-website.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip
 unzip -d ~/.font NotoSansCJKjp-hinted.zip
 
+mkdir -p resources
+wget "http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg?p=*full-image" -O resources/alice-mask.jpg
+
 wget  http://www.genpaku.org/alice01/alice01j.txt -P /tmp
 iconv -f SJIS -t UTF8 /tmp/alice01j.txt > /tmp/alice01j_utf8.txt
 hdfs dfs -put /tmp/alice01j_utf8.txt /tmp/
-mkdir -p resources
-wget "http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg?p=*full-image" -O resources/alice-mask.jpg
